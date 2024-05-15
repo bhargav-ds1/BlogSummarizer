20e20f3db79c6e9a158f41ff876f75868399e265
bhargav-ds1 <bhargav.vankayalapati3@gmail.com>
2024-05-15 19:29:09 +0200

    initialize_observability.py documentation

--- Observability/initialize_observability.py
+++ Observability/initialize_observability.py
@@ -6,6 +6,9 @@ from llama_index.core import set_global_handler
 
 
 class DefaultObservability:
+    """
+            A Class which acts a base class to provide the default and available observability providers.
+    """
     observ_provider = 'phoenix'
     observ_providers = ['deepeval', 'simple', 'phoenix']
 
@@ -13,24 +16,21 @@ class DefaultObservability:
 # optimize class design
 class InitializeObservability(DefaultObservability):
     """
-        A simple bank account class that allows deposits, withdrawals, and balance checks.
-
-        Attributes:
-        - owner (str): Account owner's name.
-        - balance (float): Account balance in dollars.
+        A Class to encapsulate different observability initializations based on provider.
 
         Constructor Parameters:
-        - owner (str): The name of the account owner.
-        - balance (float, optional): Initial balance of the account. Default is 0.0.
+        - observ_provider (Optional[str]): The name of the observability provider. Default is 'phoenix'
 
 
         Examples:
-
+            - (call) InitializeObservability('phoenix')
         Notes:
-        This class does not handle currency types or transaction histories, and should not be used for actual financial records.
-        """
+    """
 
     def __init__(self, observ_provider: Optional[str] = 'phoenix') -> None:
+        """
+                    Initializes the class with the observability provider name and calls the respective method.
+        """
         self.observ_provider = observ_provider
         if self.observ_provider not in self.observ_providers:
             raise ValueError('Observability provider should be one of ' + ','.join(self.observ_providers))
@@ -44,16 +44,7 @@ class InitializeObservability(DefaultObservability):
     @staticmethod
     def initializeDeepEval() -> None:
         """
-            Initialize LLM observability with deepeval platform.
-
-            Parameters:
-
-            Returns:
-
-            Examples:
-
-            Notes:
-
+            Initialize LLM observability with deepeval platform as observability provider.
         """
         from llama_index.callbacks.deepeval import deepeval_callback_handler
         from llama_index.core.callbacks import CallbackManager
@@ -63,49 +54,23 @@ class InitializeObservability(DefaultObservability):
     @staticmethod
     def initializeSimple() -> None:
         """
-                    Initialize LLM observability with deepeval platform.
-
-                    Parameters:
-
-                    Returns:
-
-                    Examples:
-
-                    Notes:
-
-                """
+                    Initialize the default LLM observability provided by llama-index.
+        """
         set_global_handler('simple')
 
     @staticmethod
     def initializePhoenix() -> None:
         """
-                    Initialize LLM observability with deepeval platform.
+                   Initialize LLM observability with phoenix platform as observability provider.
 
-                    Parameters:
-
-                    Returns:
-
-                    Examples:
-
-                    Notes:
-
-                """
+        """
         px.launch_app()
         llama_index.core.set_global_handler("arize_phoenix")
 
     def collect_save_traces(self) -> None:
         """
-                    Initialize LLM observability with deepeval platform.
-
-                    Parameters:
-
-                    Returns:
-
-                    Examples:
-
-                    Notes:
-
-                """
+            Saves the traces captured by the observability provider. Currently works for phoenix.
+        """
         if self.observ_provider == 'phoenix':
             file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                                      'Tests/phoenix_span_dataset.csv')
