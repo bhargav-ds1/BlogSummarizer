23ea9ee26db9d9f3e0dffefbd0400be7d5e023bc
bhargav-ds1 <bhargav.vankayalapati3@gmail.com>
2024-05-16 09:36:46 +0200

    llm_model_provider.py documentation

--- SummaryGen/llm_model_provider.py
+++ SummaryGen/llm_model_provider.py
@@ -3,11 +3,36 @@ from llama_index.core.llms.mock import MockLLM
 
 
 class LLMProvider:
+    """
+        A class to provide different implementations of large language models (LLMs) based on the specified provider.
+
+        This class allows the selection and configuration of various LLMs such as those from HuggingFace, OpenAI, or custom
+        implementations, with support for specific parameters like context window size, cache management, and token limits.
+
+        Attributes:
+            llm_provider (str): The name of the provider from which to source the LLM.
+            llm_model_name (str): The name of the model to be used.
+            llm_model_path (str): The filesystem path to the model, if using locally stored LLM and not loading from a
+                                    standard source.
+            offload_dir (str): Directory to offload parts of the model to manage memory usage.
+            cache_dir (str): Directory to cache parts of the model to avoid re-downloading.
+            local_files_only (bool): Flag to restrict loading models to local files only.
+            context_window (int): The maximum number of tokens the model can consider in a single request.
+            max_new_tokens (int): The maximum number of new tokens to be generated in the model's responses.
+            generate_kwargs (dict): Additional keyword arguments which helps control LLM response generation.
+            tokenizer_max_length (int): Maximum length of tokens for the tokenizer.
+            stopping_ids (tuple[int]): Tuple of token IDs used to indicate the end of generation.
+    """
+
     def __init__(self, llm_provider: str, llm_model_name: str, llm_model_path: str = None,
                  offload_dir: str = './offload_dir', cache_dir: str = None,
                  local_files_only: bool = False, context_window: int = 4096, max_new_tokens: int = 256,
                  generate_kwargs: dict = None, tokenizer_max_length: int = 4096,
                  stopping_ids: tuple[int] = (50278, 50279, 50277, 1, 0)) -> None:
+        """
+            Initializes the LLMProvider class with provided arguments and provides default values which are tested with
+             a local Llama2 model downloaded from huggingface .
+        """
         self.llm_provider = llm_provider
         self.llm_model_name = llm_model_name
         self.llm_model_path = llm_model_path
@@ -22,17 +47,11 @@ class LLMProvider:
 
     def get_llm_model(self) -> LLM:
         """
-                    Initialize LLM observability with deepeval platform.
+            Retrieves an LLM based on the specified provider and configuration.
 
-                    Parameters:
-
-                    Returns:
-
-                    Examples:
-
-                    Notes:
-
-                """
+                Returns:
+                    LLM: An instance of an LLM class tailored to the specified configurations and provider.
+        """
         # option to use llm from different sources, HuggingFace, Langchain, AWS, etc.
         # API provided by Together-AI is used to build and test this project
         llm = MockLLM()
