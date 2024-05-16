8a2d5a6cce1ea32c0e14b58b98713e384c7d8279
bhargav-ds1 <bhargav.vankayalapati3@gmail.com>
2024-05-16 10:01:50 +0200

    sample_test_case_generator.py documentation

--- Tests/sample_test_case_generator.py
+++ Tests/sample_test_case_generator.py
@@ -7,17 +7,13 @@ import pandas as pd
 
 def make_simple_eval_dataset() -> EvaluationDataset:
     """
-                Initialize LLM observability with deepeval platform.
+        Generates a simple evaluation dataset containing a predefined LLM test case.
 
-                Parameters:
-
-                Returns:
-
-                Examples:
-
-                Notes:
-
-            """
+            Returns:
+                EvaluationDataset: A dataset containing one test case/cases with predefined input and actual output.
+            Notes:
+                Requires filling in appropriate details to specify a valid LLMTestCase.
+    """
     test_case = LLMTestCase(
         input="What if these shoes don't fit?",
         actual_output="We offer a 30-day full refund at no extra cost.",
@@ -29,17 +25,19 @@ def make_simple_eval_dataset() -> EvaluationDataset:
 
 def make_random_blog_eval_dataset(num_queries: int = 4) -> EvaluationDataset:
     """
-                Initialize LLM observability with deepeval platform.
+        Generates an evaluation dataset containing random blog summaries. Gets random blog titles from the list of
+        titles and generates summaries for them. The data required for generating the LLMTestCases is acquired from the
+        traces logged by the observability provider (Currently defaults to phoenix).
 
-                Parameters:
+            Parameters:
+                num_queries (int): The number of random queries to generate summaries for.
 
-                Returns:
-
-                Examples:
-
-                Notes:
-
-            """
+            Returns:
+                EvaluationDataset: A dataset containing test cases generated from random blog summaries.
+            Notes:
+                Useful to generate EvaluationDataset specifically to test the blog summarization and to evaluate the
+                summarization performance using relevant metrics.
+    """
     from SummaryGen.blog_summarizer import DocumentSummaryGenerator
     from config import Config
     import random
@@ -64,17 +62,21 @@ def make_random_blog_eval_dataset(num_queries: int = 4) -> EvaluationDataset:
 def make_eval_dataset_from_phoenix_df(span_df: pd.DataFrame = None,
                                       remove_duplicates: bool = True) -> EvaluationDataset:
     """
-                Initialize LLM observability with deepeval platform.
+        Generates an evaluation dataset from a Phoenix span data frame containing trace data.
 
-                Parameters:
+            Parameters:
+                span_df (pd.DataFrame): DataFrame containing span data from Phoenix traces. If None, it attempts to
+                    fetch the trace data.
+                remove_duplicates (bool): Flag to remove duplicate entries based on LLM input messages. This is
+                    required to get the latest trace which contains full response. As, the LLM may receive multiple
+                    calls for each summarization task.
 
-                Returns:
+            Returns:
+                EvaluationDataset: A dataset of test cases constructed from trace data.
 
-                Examples:
-
-                Notes:
-
-            """
+            Raises:
+                Exception: If Phoenix client is not initialized or does not contain valid spans.
+    """
     test_cases = []
     if span_df is None:
         try:
